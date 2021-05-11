580dad411eaaa88dcac08a8e13ba0db338213d07
AMIT KUMAR MISHRA <71893015+Amit0617@users.noreply.github.com>
2021-05-11 20:13:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -88,5 +88,5 @@ function closeLinkForm() {
 }
 
 function Delete_buttons() {
-  document.querySelectorAll(".hide").style.display = "none";
+  document.getElementsByClassName("hide").style.display = "none";
 }
